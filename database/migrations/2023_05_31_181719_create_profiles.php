fec11d3d6acc1b042165fdeca69758eb74c2d9f9
miraj <mirajulislam0724@gmail.com>
2023-06-12 21:38:55 +0600

    assignment module 17

---
+++ database/migrations/2023_05_31_181719_create_profiles.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('city', 50);
+            $table->string('mobile', 20);
+            $table->string('shippingAddress', 1000);
+            $table->unsignedBigInteger('user_id');
+            $table->index('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+
+
+            // $table->foreign('email')
+            //     ->references('email')
+            //     ->on('users')
+            //     ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
